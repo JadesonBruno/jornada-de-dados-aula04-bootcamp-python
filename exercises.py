b99c31308dc1d241d605f35d7739142fda66d917
JadesonBruno <jadesonbruno.a@outlook.com>
2025-03-04 09:38:56 -0300

    :sparkles: feat: Adding resolution of 3rd  exercise

--- exercises.py
+++ exercises.py
@@ -14,5 +14,10 @@ programming_language.append("Ruby")
 print(programming_language) """
 
 # 3. Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
+""" livro = {"titulo": "1984", "autor": "George Orwell", "ano": 1949}
+
+for k, v in livro.items():
+    print(f"{k}: {v}") """
+
 # 4. Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
 # 5. Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
